c0193310b45ded4fc9201f684c4db71eaeeea75a
Lino Meert <lino.meert@gmail.com>
2019-08-09 11:31:01 +0200

    db update

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -42,6 +42,6 @@ class CocktailsController < ApplicationController
   def cocktail_params
     # *Strong params*: You need to *whitelist* what can be updated by the user
     # Never trust user data!
-    params.require(:cocktail).permit(:name, :ingredients)
+    params.require(:cocktail).permit(:name, :ingredients, :photo)
   end
 end
